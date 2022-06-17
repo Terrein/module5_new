ab1d2edbc351c10a34a5070f73aca37e598ad001
Ernest <71286606+Terrein@users.noreply.github.com>
2022-06-17 14:50:28 +0300

    change_task_2

--- prular_form.py
+++ prular_form.py
@@ -1,12 +1,3 @@
-# from unittest import result
-# import pymorphy2
-# morph = pymorphy2.MorphAnalyzer()
-# count = 3
-# expample_word = 'яблоко'
-# test = morph.parse(expample_word)[0]
-# change = test.make_agree_with_number(count).word
-# print(change)
-
 def prular_form(count, word_1,word2 = '',word3 = ''):
     """ возвращает корректную форму существительного
     в зависимости от переданного числа
@@ -16,10 +7,12 @@ def prular_form(count, word_1,word2 = '',word3 = ''):
     :param word_3: другая форма слова (необязательный атрибут)
     """
     from pymorphy2 import MorphAnalyzer
+    from transliterate import translit
     morph = MorphAnalyzer()
     expample_word = morph.parse(word_1)[0].normal_form
     transform_word = morph.parse(expample_word)[0]
     result = transform_word.make_agree_with_number(count).word
-    return result
+    translite_result = translit(result, 'ru', reversed = True)
+    return translite_result
 
-print(prular_form (115,'яблоко'))
+print(prular_form (115,'студентов'))
